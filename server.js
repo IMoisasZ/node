babe6b32ae5255ae3156e9a45551ea177b2fc188
moisas <mopri08@gmail.com>
2023-10-08 21:00:22 -0400

    API node com postgres

---
+++ server.js
@@ -0,0 +1,61 @@
+// import { createServer } from 'http'
+
+// const server = createServer((request, response) => {
+// 	response.write('I need talking in english')
+// 	return response.end()
+// })
+
+// server.listen(3333)
+import fastify from 'fastify'
+// import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgres.js'
+const server = fastify()
+
+// const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+server.post('/videos', async (request, replay) => {
+	const { title, description, duration } = request.body
+
+	await database.create({
+		title,
+		description,
+		duration,
+	})
+
+	return replay.status(201).send()
+})
+
+server.get('/videos', async (request) => {
+	const search = request.query.search
+
+	const videos = await database.list(search)
+
+	return videos
+})
+
+server.put('/videos/:id', async (request, replay) => {
+	const videoId = request.params.id
+
+	const { title, description, duration } = request.body
+
+	database.update(videoId, {
+		title,
+		description,
+		duration,
+	})
+
+	return replay.status(204).send()
+})
+
+server.delete('/videos/:id', (request, replay) => {
+	const videoId = request.params.id
+
+	database.delete(videoId)
+
+	return replay.status(204).send()
+})
+
+server.listen({
+	port: process.env.PORT ?? 3333,
+})
